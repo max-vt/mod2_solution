b0f4212d6f2970f3608ea0ac0558cce394b83b3e
max-vt <maxim.the.user@gmail.com>
2016-09-25 22:39:40 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,34 +1,81 @@
 (function () {
 'use strict';
 
-angular.module('LunchCheck', [])
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyShoppingController', ToBuyShoppingController)
+.controller('AlreadyBoughtShoppingController', AlreadyBoughtShoppingController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-.controller('LunchCheckController', LunchCheckController);
+ToBuyShoppingController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyShoppingController(ShoppingListCheckOffService) {
+  var listToBuy = this;
 
-LunchCheckController.$inject = ['$scope'];
-function LunchCheckController($scope) {
-  	$scope.check = function() { 
-  		var menu = $scope.lunch_menu;
-  		if (!menu) {
-  			$scope.message = {text: "Please enter data first", color: "red"};
-  		} else if (countItems(menu) <= 3) {
-  			$scope.message = {text: "Enjoy!", color: "green"};
-  		} else {
-  			$scope.message = {text: "Too much!", color: "green"};
-  		}
-  	};
-  	/* we are not consider empty items */
-  	function countItems(menu) {
-  		var list = menu.split(',');
-  		var number = 0;
-  		for (var i in list) {
-  			if (list[i].trim() !== '') {
-  				++number;
-  			}
-  		}
-  		return number;
-  	}
+  listToBuy.items = ShoppingListCheckOffService.getItemsToBuy();
+
+  listToBuy.buyItem = function (itemIndex) {
+    var item = listToBuy.items[itemIndex];
+    ShoppingListCheckOffService.addBoughtItem(item.name, item.quantity);
+    ShoppingListCheckOffService.removeBoughtItem(itemIndex);
+  }
 }
 
+
+AlreadyBoughtShoppingController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtShoppingController(ShoppingListCheckOffService) {
+  var listBought = this;
+
+  listBought.items = ShoppingListCheckOffService.getItemsBought();
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var items_to_buy = [
+    {
+      name: 'Apples',
+      quantity: 5
+    },
+    {
+      name: 'Bananas',
+      quantity: 3
+    },
+    {
+      name: 'Oranges',
+      quantity: 5
+    },
+    {
+      name: 'Pineapple',
+      quantity: 1
+    },
+    {
+      name: 'Mango',
+      quantity: 2
+    }
+  ];
+  // List of bought items
+  var items_bought = [];
+
+  service.addBoughtItem = function (itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+    items_bought.push(item);
+  };
+
+  service.removeBoughtItem = function (itemIdex) {
+    items_to_buy.splice(itemIdex, 1);
+  };
+
+  service.getItemsToBuy = function () {
+    return items_to_buy;
+  };
+
+  service.getItemsBought = function () {
+    return items_bought;
+  };
+}
 
 })();
